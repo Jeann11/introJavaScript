de10f455c222cc84b07c405527a8a356c7f1c24b
diegoFactoriaf5 <diego.cordoba@factoriaf5.org>
2022-12-19 13:05:29 +0100

    arrays y bucles

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 //Reserva de memoria, donde guardo informacion. La informacion puede cambiar (en tiempo de ejecucion)
 
 //var
-var name = "Adrian"
+var name = "Adrian";
 
-let number = 1
+let number = 1;
 
 //lets
-let pet = "Dog"
+let pet = "Dog";
 
 //const
 const lastName = "Cordoba";
@@ -20,10 +20,9 @@ var name = "Diego";
 number++;
 number--;
 
-
 //concatenacion
 //console.info("el nombre del facilitador " +  name + " " + "y tengo un " + pet)
- 
+
 //interpolacion
 //console.info(`mi nombre es ${name} y tengo un ${pet}`)
 
@@ -37,147 +36,71 @@ number--;
 
 //Funciones>
 //Una función es un bloque de instrucciones que puede ejecutarse tantas veces como desee.
-//Una función puede tener parámetros y devolver un valor. Las funciones se pueden definir de diferentes 
+//Una función puede tener parámetros y devolver un valor. Las funciones se pueden definir de diferentes
 //maneras. Quizás la más común es la declaración de función. Comienza con la palabra clave function. Sigue:
 //El nombre de la función,los parámetros encerrados entre paréntesis, las afirmaciones encerradas entre corchetes.
 
-
-
-
-
-
-
-
-
-
-
-
-
-//declara una funcion
-function medirSuperficieCuadrado(longitudLado) {
-    return longitudLado * longitudLado;
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Variables 
-let numeroA = 3 
-let numeroB = 6
-let numeroZ = 7
+//Variables
+let numeroA = 3;
+let numeroB = 6;
+let numeroZ = 7;
 
 //Declarar funciones
-function sumar(Numero1, raul, diego){
-  return Numero1 + Numero2 + diego
+function sumar(Numero1, Numero2) {
+  return Numero1 + Numero2;
 }
 
-function restar(a , b){
-  return a - b
+function restar(a, b) {
+  return a - b;
 }
 
-function multiplicar(a , b){
-  return a * b
+function multiplicar(a, b) {
+  return a * b;
 }
-
 
 //llamar a las funciones
-//console.info(sumar(3567, numeroA, numeroZ)) //10
-//console.info(sumar(6, 15, numeroB)) //21 //hardcode - no lo hagan
-
-
-
-
-
-
-
-
-
-
-
-
-
-var longitudLado = 6
-
-
-//llamar a una funcion
-//var resultado = medirSuperficieCuadrado(longitudLado)
-
-//console.info(resultado)
-
-
-
-
-
-
-
-
-
-
-
-//-----------------------------------------------------------------------------------------------//
-
-//Ejercicios//
-//1
-//Defina una función helloque devuelva 'Hello world!'. 
-//Imprimirla en consola con un string que nos calare que ejercicio es.
-
-//2
-//Defina dos funciones.
-//La primera función adebería devolver 'Hello a!'y la segunda función bdebería devolver 'Hello b!'.
-//Imprimirla en consola con un string que nos calare que ejercicio es.
-
-//3
-//Defina una función greet que devuelva el valor 'Haydo!'.
-//Declarar una variable salutation. 
-//Llame a la función greety asigne el resultado de la llamada a la variable salutation.
-//Imprimirla en consola con un string que nos calare que ejercicio es.
-
+//console.info(sumar(3567, numeroA, numeroZ)); //10
+//console.info(sumar(6, 15, numeroB)); //21 //hardcode - no lo hagan
 
 //Parámetros
+//definimos una función que simplemente devuelve el parámetro input. Los parámetros son variables.
 
-function give(numeroA) {
-  return numeroA + 5;
-}
-const resultado = give(6)
+//llamamos a la función y le pasamos el argumento 'apple'. La función será ejecutada.
 
-console.info("este es esl resultado de give" + give(5))
-console.info(resultado)
-
-//definimos una función que simplemente devuelve el parámetro input. Los parámetros son variables. 
-
-let result = give('apple');
-
-//llamamos a la función y le pasamos el argumento 'apple'. La función será ejecutada. 
-
-//El argumento 'apple'se asignará al parámetro input. Input obtiene el valor 'apple'. 
-//La variable input se devuelve en la única línea de código de la función. 
+//El argumento 'apple'se asignará al parámetro input. Input obtiene el valor 'apple'.
+//La variable input se devuelve en la única línea de código de la función.
 //Este retorno se asigna a la variable result. Finalmente, result tiene el valor 'apple'.
 
-//Ejercicio
-//Escribir una función echo que también devuelva el parámetro pasado. 
-//echo('Greta') debe volver 'Greta'y echo('CO2') debe volver'CO2'
+
 
 
 //CONDICIONALES ------- que son???
-//ARRAYS ------que son??? que funciones existen para maniular Arrays?
+
+let edadDeRaul = 17;
+let edadDeLucia = 20;
+let edadDeCarlos = 89;
+const nombre = "Raul";
+
+function validarEdad(edad) {
+  var resultado;
+  switch (edad) {
+    case edad >= 18:
+      resultado = " puede conducir";
+      break;
+    case edad >= 70:
+      resultado = " deja la herencia y no conduzcas";
+      break;
+    case edad >= 80:
+      resultado = " deja la herencia y no conduzcas";
+      break;
+    default:
+      resultado = " no puede conducir tampoco";
+  }
+
+  return resultado;
+}
+
+//console.info(validarEdad(edadDeCarlos));
 
 
 
@@ -186,4 +109,34 @@ let result = give('apple');
 
 
 
-//medirSuperficieCuadrado()
+//Bucles
+
+//for repite la misma accion asta que sale del bucle
+
+//horas trabajadas
+
+for(let horas = 0; horas <= 40; horas++){
+  console.info("llevas " + horas + " trabajadas")
+}
+
+
+
+//ARRAYS ------que son??? que funciones existen para manipular Arrays?
+
+
+//Arrays [] lista o conjunto grupo de datos, los datos pueden ser de varios tipos.
+
+
+//forEach atado a los Arrays
+
+let listaDeNumeros= [5, 78, 24, 567]
+
+listaDeNumeros.forEach(function(numero){
+
+  if(numero < 50){
+    console.info(numero)
+  }
+  console.info("numero mayor a 50 por eso no lo imprimo")
+})
+
+console.info(listaDeNumeros[3])
