4eec495e45dc636fc3deb8e4eae9b1adc3fa76e2
Jeann11 <jeanneth8464@gmail.com>
2022-12-19 20:28:33 +0100

    iris.js

---
+++ iris.js
@@ -0,0 +1,16 @@
+
+function app() {
+    const nameC = "Pepa"
+    let name = "Iris"
+
+    let name1 = "Pepe"
+    name2 = "Lola"
+    name3 = "Eva"
+    
+
+    // console.log( name + " " + name3 );
+    console.log( `${name3} ${name}` );
+
+}
+
+app()
